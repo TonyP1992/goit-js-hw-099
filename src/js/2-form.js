7ad35dc9316b3919556b2af700714b6419c9076f
TonyP1992 <okpetrakov@gmail.com>
2025-09-08 16:04:46 +0800

    add form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,40 +1,51 @@
-const formEl = document.querySelector('.feedback-form');
+let formData = { email: '', message: '' };
 
-const formData = {
-  email: '',
-  message: '',
-};
-formEl.addEventListener('input', e => {
-  const email = e.currentTarget.elements.email.value;
-  const message = e.currentTarget.elements.message.value;
-  formData.email = email;
-  formData.message = message;
+document.addEventListener('DOMContentLoaded', () => {
+  const formElem = document.querySelector('.feedback-form');
 
-  saveToLS('formData', formData);
-});
+  const lsData = getFromLS('feedback-form-state', { email: '', message: '' });
 
-// ============================================
-document.addEventListener("DOMContentLoaded", ()=>{
-    const lsData = getFromLs("formData");
-    try {
-        formData = lsData;
-formEl.elements.email.value = lsData.email;
-    formEl.elements.message.value = lsData.message;
-    } catch {
-        console.log("Error!");
-        
+  if (lsData && typeof lsData === 'object') {
+    formData.email = lsData.email;
+    formData.message = lsData.message;
+  } else {
+    formData.email = '';
+    formData.message = '';
+  }
+  formElem.elements.email.value = formData.email;
+  formElem.elements.message.value = formData.message;
+
+  formElem.addEventListener('input', e => {
+    formData.email = e.currentTarget.elements.email.value.trim();
+    formData.message = e.currentTarget.elements.message.value.trim();
+    saveToLS('feedback-form-state', formData);
+  });
+
+  formElem.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+    if (!email || !message) {
+      alert('Не заповнені дані');
+      return;
     }
-    
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    formElem.reset();
+    formData = { email: '', message: '' };
+  });
 });
 
-// ============================================
 function saveToLS(key, value) {
   const jsonData = JSON.stringify(value);
   localStorage.setItem(key, jsonData);
 }
 
-function getFromLs(key, defaultValue) {
+function getFromLS(key, defaultValue) {
   const jsonData = localStorage.getItem(key);
+  if (jsonData === null) {
+    return defaultValue;
+  }
   try {
     const data = JSON.parse(jsonData);
     return data;
@@ -42,3 +53,4 @@ function getFromLs(key, defaultValue) {
     return defaultValue || jsonData;
   }
 }
+
